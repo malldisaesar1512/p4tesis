158cec3519aaee24103d16db6c75a2c886254d6e
malldisaesar1705 <back.me1705@gmail.com>
2025-04-26 07:51:25 +0700

    add routing send DBD_

--- Final/sendOSPF.py
+++ Final/sendOSPF.py
@@ -95,9 +95,8 @@ def handle_incoming_packet(packet):
        return
    
    ospfhdr_layer = packet.getlayer(OSPF_Hdr)
-   ospfhdr_layer2 = packet.getlayer(OSPF_Hello)
    
-   if ospfhdr_layer.type == 1 | ospfhdr_layer.type == 2 & ospfhdr_layer2.neighbors != 0: # Hello Packet
+   if ospfhdr_layer.type == 1: # Hello Packet
        # Paket hello diterima -> kirim DBD sebagai respons ke source IP di layer IP 
        src_ip_of_neighbor = packet[IP].src  
        print(f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] Received HELLO from {src_ip_of_neighbor}, sending DBD...")
