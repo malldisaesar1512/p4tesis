a34cf4560b2862f8e97382ea60add1f78ce491e1
malldisaesar1705 <back.me1705@gmail.com>
2025-04-28 22:06:26 +0700

    state loading

--- Final/ospfrouting.py
+++ Final/ospfrouting.py
@@ -4,6 +4,28 @@ import time
 import threading
 import random
 
+# Konfigurasi parameter OSPF
+router_id = "10.10.1.2"  # Router ID
+router_id2 = "192.168.1.1"  # Router ID 2 (Neighbor)
+area_id = "0.0.0.0"        # Area ID
+interface = "ens5"         # Network interface
+
+# Variabel untuk melacak state neighbor
+neighbor_state = "Init"
+neighbor_ip = router_id2
+dbd_seq_num = random.randint(10000, 50000)
+dbd_seq_num_neighbor = None
+master = False
+
+# Membuat paket Ethernet
+eth = Ether()
+
+# Membuat paket IP dengan destination multicast address OSPF (224.0.0.5)
+ip = IP(src=router_id, dst="224.0.0.5")
+
+# Membuat header OSPF (versi 2, tipe 1=Hello)
+ospf_header = OSPF_Hdr(version=2, type=1, src=router_id, area=area_id)
+
 def send_ospf_lsr(neighbor_ip):
     """Kirim paket Link State Request (LSR) ke neighbor"""
     # Header IP unicast ke neighbor router IP
