6f14c9ae4a7f2a2a7ec10d6332d9d6fc13015397
malldisaesar1705 <back.me1705@gmail.com>
2025-05-01 23:25:50 +0700

    fix after full state

--- Final/sendOSPF.py
+++ Final/sendOSPF.py
@@ -389,7 +389,7 @@ def handle_incoming_packet(packet):
         src_ip_of_neighbor = packet[IP].src
         lsu_layer = packet.getlayer(OSPF_LSUpd)
         print(f"LSU Layer: {lsu_layer}")
-        lsu_id = lsu_layer.lsaheaders
+        lsu_id = lsu_layer.lsalist[0].id
         print(f"LSU ID: {lsu_id}")
         lsu_adrouter = lsu_layer.adrouter
         lsu_seq = lsu_layer.seq
