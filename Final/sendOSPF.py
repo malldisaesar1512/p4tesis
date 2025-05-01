7cf744c88a08bad3f7f88a16c49a36762aadb3df
malldisaesar1705 <back.me1705@gmail.com>
2025-05-01 21:25:48 +0700

    fix after full state

--- Final/sendOSPF.py
+++ Final/sendOSPF.py
@@ -403,7 +403,7 @@ def handle_incoming_packet(packet):
 
 def sniff_packets():
    print("Sniffing packets...")
-   sniff(iface=interface , filter="ip proto ospf", prn=lambda pkt: handle_incoming_packet(pkt), store=False, timeout=5 )
+   sniff(iface=interface , filter="ip proto ospf", prn=lambda pkt: handle_incoming_packet(pkt), store=False, timeout=10 )
    
 
 if __name__ == "__main__":
