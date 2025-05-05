4d280909cef10663f8acf0f4c4b31569c5f0b2d7
malldisaesar1705 <back.me1705@gmail.com>
2025-05-05 23:00:41 +0700

    fix exchange

--- Final/ospfrouting.py
+++ Final/ospfrouting.py
@@ -310,7 +310,7 @@ def handle_incoming_packet(packet):
                     for i in range(jumlah_lsa):
                         lsa = dbd_layer.lsaheaders[i]
                         lsadb_list.append(lsa)
-                        print(f"LSA {i+1}: ID: {lsa.id}, Type: {lsa.type}, Advertising Router: {lsa.adrouter}, Sequence Number: {lsa.seq}")
+                        # print(f"LSA {i+1}: ID: {lsa.id}, Type: {lsa.type}, Advertising Router: {lsa.adrouter}, Sequence Number: {lsa.seq}")
                     
                     send_ospf_lsr(src_ip)
 
