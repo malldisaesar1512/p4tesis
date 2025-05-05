51343b72f6504effc77c65757f856e107a1bfeee
malldisaesar1705 <back.me1705@gmail.com>
2025-05-05 22:19:57 +0700

    fix exchange

--- Final/ospfrouting.py
+++ Final/ospfrouting.py
@@ -291,6 +291,9 @@ def handle_incoming_packet(packet):
                         # send_ospf_dbd_first(src_ip, seq_random)
                         send_ospf_dbd(src_ip)
                         print(f"Sent DBD packet to {src_ip} at {time.strftime('%Y-%m-%d %H:%M:%S')} - State: {neighbor_state}")
+                    for i in range(jumlah_lsa):
+                        lsa = dbd_layer.lsaheaders[i]
+                        print(f"LSA {i+1}: ID: {lsa.id}, Type: {lsa.type}, Advertising Router: {lsa.adrouter}, Sequence Number: {lsa.seq}")
             # elif neighbor_state == "Exchange":
             #     if src_ip != router_id:
             #         if router_status == "Master":
