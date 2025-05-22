5b7edf84784cddc4234e55adebe32d0a6b3ecda4
malldisaesar1705 <back.me1705@gmail.com>
2025-05-22 15:02:12 +0700

    benerin code ospf

--- Final/addroute_mikrotik.py
+++ Final/addroute_mikrotik.py
@@ -1,12 +1,18 @@
 from librouteros import connect
+from librouteros.exceptions import TrapError
 
 def add_route_to_mikrotik(ip, username, password, dst, gateway):
-    api = connect(username=username, password=password, host=ip)
-    api('/ip/route/add', **{
-        'dst-address': dst,
-        'gateway': gateway
-    })
-    print(f'Route {dst} via {gateway} added to Mikrotik {ip}')
+    try:
+        api = connect(username=username, password=password, host=ip)
+        api('/ip/route/add', **{
+            'dst-address': dst,
+            'gateway': gateway
+        })
+        print(f'Route {dst} via {gateway} added to Mikrotik {ip}')
+    except TrapError as e:
+        print(f'Failed to add route {dst} via {gateway} to Mikrotik {ip}: {e}')
+    except Exception as e:
+        print(f'Unexpected error: {e}')
 
 if __name__ == "__main__":
     ip="10.10.1.1"
