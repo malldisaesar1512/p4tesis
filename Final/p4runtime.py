b78a92bf4f812ba88cd19e06f0acfaca238371e6
malldisaesar1705 <back.me1705@gmail.com>
2025-05-28 16:43:37 +0700

    coba tambah p4runtime

--- Final/p4runtime.py
+++ Final/p4runtime.py
@@ -8,9 +8,9 @@ import p4runtime_sh.shell as sh
 logger = logging.getLogger(None)
 logger.setLevel(logging.INFO)
 
-grpc_addrp4 = 'localhost:9559'
-p4info_path = 'forwarding.p4info.txtpb'
-json_path = 'finalospf.json'
+grpc_addrp4 = '0.0.0.0:9559'
+p4info_path = './forwarding.p4info.txtpb'
+json_path = './finalospf.json'
 # you can omit the config argument if the switch is already configured with the
 # correct P4 dataplane.
 sh.setup(
@@ -26,7 +26,7 @@ logger.info('P4Runtime shell setup complete.')
 te = sh.TableEntry('MyIngress.ipv4_lpm')(action='MyIngress.ipv4_forward',)
 te.match['hdr.ipv4.dstAddr'] = '10.10.2.0/24'
 te.action['dstAddr'] = '50:00:00:00:30:00'
-te.action['port'] = 1
+te.action['port'] = '1'
 te.insert()
 
 logger.info('Table entry inserted: %s', te)
