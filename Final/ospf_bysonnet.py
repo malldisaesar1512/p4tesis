d859c6e8bd8dfe5eb139fe32bcd2a899634f339c
malldisaesar1705 <back.me1705@gmail.com>
2025-06-01 17:36:46 +0700

    edit ospf kemarin

--- Final/ospf_bysonnet.py
+++ Final/ospf_bysonnet.py
@@ -754,8 +754,8 @@ def handle_incoming_packet(packet, interface, src_broadcast, source_ip):
                     send_ospf_lsaack(interface, src_broadcast, source_ip, broadcast_ip)
                     print(f"Sent LS_ACK packet to {src_ip} at {time.strftime('%Y-%m-%d %H:%M:%S')} - State: {neighbor_state}")
             if tracking_state.get(interface, {}).get("state") == "Full":
+                print("Received LSU packet on Full state")
                 if  ip2 in network1 and src_ip not in ips:
-                    print("Received LSU packet on Full state")
                     lsu_layer = packet.getlayer(OSPF_LSUpd)
                     jumlah_lsulsa = lsu_layer.lsacount
                     print(f"Received LSU from {src_ip}, moving to Full state")
