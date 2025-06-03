01076dc12f5aeb5438f64447e05021071e3d2e50
malldisaesar1705 <back.me1705@gmail.com>
2025-06-03 22:50:16 +0700

    edit p4reroute

--- Final/ospf_bysonnet.py
+++ Final/ospf_bysonnet.py
@@ -273,10 +273,10 @@ def add_to_p4(interface):
         write_register("linkstatus", 0, 0, 9090)  # Set link status to up
         write_register("enc_status", 0, 0, 9090)  # Set ECN status to 0
         write_register("modify_status", 0, 0, 9090)  # Set port out to 0
-        # parameter1 = f"MyIngress.ipv4_lpm MyIngress.ipv4_forward 192.168.1.3/32 => 50:00:00:00:10:00 0"
-        # parameter2 = f"MyIngress.ipv4_reroute MyIngress.ipv4_rerouting 192.168.1.3/32 => 50:00:00:00:10:00 0"
-        # table_add(parameter1, 9090)
-        # table_add(parameter2, 9090)
+        parameter1 = f"MyIngress.ipv4_lpm MyIngress.ipv4_forward 192.168.1.3/32 => 50:00:00:00:10:00 0"
+        parameter2 = f"MyIngress.ipv4_reroute MyIngress.ipv4_rerouting 192.168.1.3/32 => 50:00:00:00:10:00 0"
+        table_add(parameter1, 9090)
+        table_add(parameter2, 9090)
 
 def modify_route():
     global db_lsap4, networklist, mac_src, list_route
@@ -347,10 +347,10 @@ def modify_route():
         write_register("linkstatus", 0, 0, 9090)  # Set link status to up
         write_register("enc_status", 0, 0, 9090)  # Set ECN status to 0
         write_register("modify_status", 0, 0, 9090)  # Set port out to 0
-        # parameter1 = f"MyIngress.ipv4_lpm MyIngress.ipv4_forward 192.168.1.3/32 => 50:00:00:00:10:00 0"
-        # parameter2 = f"MyIngress.ipv4_reroute MyIngress.ipv4_rerouting 192.168.1.3/32 => 50:00:00:00:10:00 0"
-        # table_add(parameter1, 9090)
-        # table_add(parameter2, 9090)
+        parameter1 = f"MyIngress.ipv4_lpm MyIngress.ipv4_forward 192.168.1.3/32 => 50:00:00:00:10:00 0"
+        parameter2 = f"MyIngress.ipv4_reroute MyIngress.ipv4_rerouting 192.168.1.3/32 => 50:00:00:00:10:00 0"
+        table_add(parameter1, 9090)
+        table_add(parameter2, 9090)
 #################### P4 CONTROLLER #####################
 def cost_calculation(th_link, ecn_mark, rtt_link, link_status):
     BW_DEFAULT = 10000000  # Bandwidth default dalam bps
