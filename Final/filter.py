007ef2e321135bfadb9c183daa4fefc8e7d758ac
malldisaesar1705 <back.me1705@gmail.com>
2025-06-17 11:30:48 +0700

    add trafgen

--- Final/filter.py
+++ Final/filter.py
@@ -4,9 +4,11 @@ import datetime
 def monitoring_icmp(pkt):
     if ICMP in pkt and pkt[ICMP].type == 8:  # ICMP echo request (ping)
         waktu_kirim = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
-        # Mendapatkan interface keluar (outgoing interface)
         interface = pkt.sniffed_on if hasattr(pkt, "sniffed_on") else "unknown"
         print(f"[{waktu_kirim}] Paket ICMP keluar ke {pkt[IP].dst} melalui interface {interface}")
 
-# Sniff paket ICMP keluar pada semua interface
-sniff(filter="icmp and outbound", prn=monitoring_icmp, store=0)
+# Dapatkan daftar semua interface yang aktif
+interfaces = get_if_list()
+
+# Jalankan sniff di semua interface sekaligus
+sniff(iface=interfaces, filter="icmp and outbound", prn=monitoring_icmp, store=0)
